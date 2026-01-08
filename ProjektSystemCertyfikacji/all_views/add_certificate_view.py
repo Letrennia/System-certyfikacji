049f1b6d728d6e33f1b47ce2ab5ad1b51b0f8430
nate <89145@student.pb.edu.pl>
2026-01-08 17:14:29 +0100

    zmiana url v1

--- ProjektSystemCertyfikacji/all_views/add_certificate_view.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def add_certificate(request):
-    return render(request, 'company/add_certificate.html')
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from ProjektSystemCertyfikacji.all_views import views_certificate
 from django.urls import path, include
 from django.views.generic import RedirectView
 from ProjektSystemCertyfikacji.all_views.certificates_views import add_cert, cert_succes, list_cert,  cert_detail
-from ProjektSystemCertyfikacji.all_views import c_sign_in_view, c_sign_out_view, c_sign_up_view, main_page_view, add_certificate_view
+from ProjektSystemCertyfikacji.all_views import c_sign_in_view, c_sign_out_view, c_sign_up_view, main_page_view
 from ProjektSystemCertyfikacji.utils import redirect_certificate_url
 from main_app import settings
 
@@ -16,12 +16,12 @@ urlpatterns = [
     path('redirect/<str:token>/', redirect_certificate_url.redirect_certificate, name='redirect_certificate'),
     path('certificate/<path:token>/', views_certificate.certificate_view, name='certificate_view'), 
     path('report_fraud/<path:token>/', views_certificate.report_fraud, name='report_fraud'),
-    # path('certificate/<str:certificate_id>/', views_certificate.certificate_detail, name='certificate_detail'),
+
     path('login/', c_sign_in_view.sign_in, name='login'),
     path('logout/', c_sign_out_view.sign_out, name='logout'),
     path('register/', c_sign_up_view.sign_up, name='register'),
     path('main_page/', main_page_view.main_page, name='main'),
-    path('add_certificate/', add_certificate_view.add_certificate, name='add_certificate'),
+
 
     path('certificates/add/', add_cert, name='add_cert'),
     path('certificates/success/<int:cert_id>/', cert_succes, name='cert_succes'),
