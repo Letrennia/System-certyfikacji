98af8f735fd59bcdbdbbee5de70be228f929abdb
Katarzyna Cybulska <89113@student.pb.edu.pl>
2026-04-04 17:43:04 +0200

    Powiadomienia cd. (jednoskta,  producent)

--- ProjektSystemCertyfikacji/all_views/notification_view.py
+++ ProjektSystemCertyfikacji/all_views/notification_view.py
@@ -1,6 +1,4 @@
 from datetime import timedelta
-
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render
@@ -11,17 +9,41 @@ from ProjektSystemCertyfikacji.models import Certificate, Fraud_report
 @login_required
 def show_notifications(request):
     user = request.user
-    today = timezone.now()
+    today = timezone.now().date()
 
     if hasattr(user, 'company_user'):
-        cert = Certificate.objects.filter(holder_company_id=user.company_user,valid_to__gte=today,
-                                          valid_to__lte=today+timedelta(days=3)) #trzy dni do wygasniecia
-        return render(request, 'notification_tab.html', {'cert': cert})
+        expired = Certificate.objects.filter(
+            holder_company_id=user.company_user,
+            valid_to__lt=today
+        ).order_by('-valid_to')
+
+        expiring = Certificate.objects.filter(
+            holder_company_id=user.company_user,
+            valid_to__gte=today,
+            valid_to__lte=today + timedelta(days=3)
+        ).order_by('valid_to')
+
+        return render(request, 'notifications/notification_cert_unit_list.html', {
+            "expired": expired,
+            "expiring": expiring
+        })
 
     elif hasattr(user, 'certifying_unit'):
-        cert = Certificate.objects.filter(issued_by_certifying_unit_id=user.certifying_unit, valid_to__gte=today,
-                                          valid_to__lte=today+timedelta(days=3)) #trzy dni do wygasniecia
-        return render(request, 'notification_tab.html', {'cert': cert})
+        expired = Certificate.objects.filter(
+            issued_by_certifying_unit_id=user.certifying_unit,
+            valid_to__lt=today
+        ).order_by('-valid_to')
+
+        expiring = Certificate.objects.filter(
+            issued_by_certifying_unit_id=user.certifying_unit,
+            valid_to__gte=today,
+            valid_to__lte=today + timedelta(days=3)
+        ).order_by('valid_to')
+
+        return render(request, 'notifications/notification_cert_unit_list.html', {
+            'expired': expired,
+            'expiring': expiring
+        })
 
     elif user.is_staff:
         frauds = Fraud_report.objects.order_by('-created_at')
